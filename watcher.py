0ea141c48ee7ef98d1630a66ccabf7be6e3b8232
Javier <dummy@dummy.com>
2024-11-06 18:05:51 -0600

    Se crea clase Trailer_Hitch.py y se actualiza proograma serial prt

--- watcher.py
+++ watcher.py
@@ -6,23 +6,33 @@ DEBUG = True
 level = logging.DEBUG if DEBUG else logging.ERROR
 logging.basicConfig(level=level)
 
+STDMAN_FILE = 'archivotest.txt' #file location
+ROW_LAST_CHAR = 10      # ROW where is located the last character
+
 
 def watch_file(path: str):
+    logging.info(" Starting watching file %s ", path)
     for changes in watch(path, raise_interrupt=False, rust_timeout= 5000):
-        print(changes)
-        print("hubo cambvios")
+        #Poner Codigo aquui
+        character = get_last_seams(path)
+        print(character)
 
-def last_seam(file_name: str, target_line: int):
+
+def get_last_seams(file_name: str):
+    logging.debug("  Starting last_seam")
+    target_line = ROW_LAST_CHAR
     try:
         with open(file_name, 'r') as file:
-            logging.debug(f" File \"{file_name}\" founded")
+            logging.debug(f" File \"{file_name}\" opened and looking in row {target_line}")
             content = file.readlines()
-            seam_cycle = content[target_line - 1]
-            return seam_cycle
+            last_character = content[target_line - 1]   #index starts in 0
+            logging.debug(f" Last character = {last_character}")
+            return last_character
     except FileNotFoundError:
         logging.error(" File not founded")
         print("No such file or directory")
 
+
 if __name__ == '__main__':
     #print(last_seam('archivotest.txt', 10))
-    watch_file("archivotest.txt")
+    watch_file(STDMAN_FILE)
